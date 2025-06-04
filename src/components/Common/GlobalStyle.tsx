62e49ecbcc6744a174fa0e77db4248b24023bcc6
Donhyeok Kang <dh5473@naver.com>
2025-06-04 22:53:39 +0900

    Feat: pretendard font

--- src/components/Common/GlobalStyle.tsx
+++ src/components/Common/GlobalStyle.tsx
@@ -2,13 +2,28 @@ import React, { FunctionComponent } from 'react'
 import { Global, css } from '@emotion/react'
 
 const defaultStyle = css`
-  @import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap');
+  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css');
+  @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&display=swap');
 
   * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    font-family: 'Nanum Myeongjo', serif;
+    font-family:
+      'Pretendard',
+      -apple-system,
+      BlinkMacSystemFont,
+      system-ui,
+      Roboto,
+      'Helvetica Neue',
+      'Segoe UI',
+      'Apple SD Gothic Neo',
+      'Noto Sans KR',
+      'Malgun Gothic',
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Segoe UI Symbol',
+      sans-serif;
   }
 
   html,
@@ -22,6 +37,16 @@ const defaultStyle = css`
     color: inherit;
     text-decoration: none;
     cursor: pointer;
+  }
+
+  /* 코드 블록 전용 폰트 */
+  code,
+  pre,
+  code[class*='language-'],
+  pre[class*='language-'] {
+    font-family:
+      'JetBrains Mono', 'Fira Code', Consolas, 'Liberation Mono', Menlo,
+      Courier, monospace !important;
   }
 `
 
